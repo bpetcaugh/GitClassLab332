a3cafa445c4c8bfa355ee1e9008432051c683365
bpetcaughCU <bpetcaugh@cairn.edu>
2023-02-14 23:13:53 -0500

    Added string exercise

--- src/AddYourString.java
+++ src/AddYourString.java
@@ -1,11 +1,85 @@
 public class AddYourString {
-    public static void main(String[] args) {
-        System.out.println("ProfPetcaugh's Msg is: " + ProfPetcaughMsg());
-    }
-
     static String ProfPetcaughMsg() {
         return "Version control rocks!";
     }
 
+    static String NCioneMsg() {
+        return "";
+    }
 
+    static String CConradMsg() {
+        return "";
+    }
+
+    static String SDouglassMsg() {
+        return "";
+    }
+
+    static String CEelmanMsg() {
+        return "";
+    }
+
+    static String JFiaccoMsg() {
+        return "";
+    }
+
+    static String MKaromoMsg() {
+        return "";
+    }
+
+    static String EKimMsg() {
+        return "";
+    }
+
+    static String AKondratyukMsg() {
+        return "";
+    }
+
+    static String NLofgrenMsg() {
+        return "";
+    }
+
+    static String DNgangaMsg() {
+        return "";
+    }
+
+    static String LNguyenMsg() {
+        return "";
+    }
+
+    static String CRudloffMsg() {
+        return "";
+    }
+
+    static String GSeidelMsg() {
+        return "";
+    }
+
+    static String BWilliamsMsg() {
+        return "";
+    }
+
+    static String DZhaoMsg() {
+        return "";
+    }
+
+    public static void main(String[] args) {
+        //Don't touch this code
+        System.out.println("ProfPetcaugh's Msg is: " + ProfPetcaughMsg());
+        System.out.println("NCione's Msg is: " + NCioneMsg());
+        System.out.println("CConrad's Msg is: " + CConradMsg());
+        System.out.println("SDouglass's Msg is: " + SDouglassMsg());
+        System.out.println("CEelman's Msg is: " + CEelmanMsg());
+        System.out.println("JFiacco's Msg is: " + JFiaccoMsg());
+        System.out.println("MKaromo's Msg is: " + MKaromoMsg());
+        System.out.println("EKim's Msg is: " + EKimMsg());
+        System.out.println("AKondratyuk's Msg is: " + AKondratyukMsg());
+        System.out.println("NLofgren's Msg is: " + NLofgrenMsg());
+        System.out.println("DNg'ang'a's Msg is: " + DNgangaMsg());
+        System.out.println("LNguyen's Msg is: " + LNguyenMsg());
+        System.out.println("CRudloff's Msg is: " + CRudloffMsg());
+        System.out.println("GSeidel's Msg is: " + GSeidelMsg());
+        System.out.println("BWilliam's Msg is: " + BWilliamsMsg());
+        System.out.println("DZhao's Msg is: " + DZhaoMsg());
+    }
 }
