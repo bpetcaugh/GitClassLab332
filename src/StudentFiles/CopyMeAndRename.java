9b31760907cf7a487faf0af710f6fe1be1738b65
bpetcaughCU <bpetcaugh@cairn.edu>
2023-02-15 00:24:35 -0500

    Create java exercise

---
+++ src/StudentFiles/CopyMeAndRename.java
@@ -0,0 +1,25 @@
+package StudentFiles;
+
+public class CopyMeAndRename {
+    //Copy everything from here down, except the final bottom '}'
+    //Then copy that into a new file titled with your name, example: JohnSmith.java
+    public static void main(String[] args) {
+
+    }
+
+    //Website: https://edabit.com/challenge/QCgoxbd32BqFr6AY7
+    public static String fizzBuzz(int n) {
+
+        return "sample answer";
+    }
+
+    //Website: https://edabit.com/challenge/rzpucPyoyEtXPo2BG
+    public static int[] arrayOfMultiples(int num, int length) {
+
+        int[] answer = new int[1000]; //Sample line so code executes
+
+        
+        return answer;
+    }
+
+}
