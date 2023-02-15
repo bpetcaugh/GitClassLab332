d45d5f5d7788e9a99851bf8e657114adcde53bdf
bpetcaughCU <bpetcaugh@cairn.edu>
2023-02-15 09:10:42 -0500

    Added my string

--- src/AddYourString.java
+++ src/AddYourString.java
@@ -1,6 +1,6 @@
 public class AddYourString {
     static String ProfPetcaughMsg() {
-        return "Version control rocks!";
+        return "Version control rocks! Class is almost over, ahhhh";
     }
 
     static String NCioneMsg() {
